6ec7171c6431f28a85d680601baabf18d673d2b2
Mike Emmons <memmons18@gmail.com>
2020-08-21 22:27:40 -0400

    map through api call and add to card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,20 +3,20 @@ import React from "react"
 function Card(props) {
     return (
         <div className="col-3">
-            <div className="card" style={{ width: "18rem;", marginBottom: "10px" }}>
-                {/* <img src="..." className="card-img-top" alt="..."> */}
+            <div className="card" style={{ marginBottom: "10px", minHeight: "400px" }}>
+                <img src={props.name.picture.large} className="card-img-top" alt="..." />
                 <div className="card-body">
-                    <h5 className="card-title">{props.name.name}</h5>
+                    <h5 className="card-title">{props.name.name.first}{props.name.name.last}</h5>
                     <p className="card-text">
                         {props.name.email}
                     </p>
                     <p className="card-text">
-                        {props.name.phone}
+                        {props.name.cell}
                     </p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+
                 </div>
             </div>
-        </div>
+        </div >
 
     )
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,26 +2,44 @@ import React from "react"
 import Header from "./Header"
 import Search from "./Search"
 import Card from "./Card"
+import API from "../utils/API"
 
-function MainComponent() {
-    const names = [{ name: "unknown", email: "anon@gmail.com", phone: "234-532-5678" }, { name: "unknown", email: "anon@gmail.com", phone: "234-532-5678" }, { name: "unknown", email: "anon@gmail.com", phone: "234-532-5678" }, { name: "unknown", email: "anon@gmail.com", phone: "234-532-5678" }, { name: "unknown", email: "anon@gmail.com", phone: "234-532-5678" }, { name: "unknown", email: "anon@gmail.com", phone: "234-532-5678" }, { name: "unknown", email: "anon@gmail.com", phone: "234-532-5678" }]
-    return (
-        <div>
-            <Header />
+class MainComponent extends React.Component {
+    state = {
+        results: [],
+        search: "",
+        category: ""
+    }
 
-            <main className="row">
-                <aside className="col-4">
-                    <Search />
-                </aside>
-                <section className="col-8">
-                    <div className="row">
-                        {names.map(item => {
-                            return <Card name={item} />
-                        })}
-                    </div>
-                </section>
-            </main>
-        </div>
-    )
+
+    componentDidMount = () => {
+        API.getUsers().then(results => {
+            this.setState({
+                results: results.data.results
+            })
+        })
+
+    }
+
+    render() {
+        return (
+            <div>
+                <Header />
+
+                <main className="row">
+                    <aside className="col-4">
+                        <Search />
+                    </aside>
+                    <section className="col-8">
+                        <div className="row">
+                            {this.state.results.map(item => {
+                                return <Card name={item} />
+                            })}
+                        </div>
+                    </section>
+                </main>
+            </div>
+        )
+    }
 }
 export default MainComponent;

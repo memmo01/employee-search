9a8c095544ccfec1786e7b9b6d59f04474c59bec
Mike Emmons <memmons18@gmail.com>
2020-09-04 20:59:42 -0400

    convert class components to function components

--- src/components/MainComponent/index.js
+++ src/components/MainComponent/index.js
@@ -1,84 +1,79 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Header from "../Header"
 import Search from "../Search"
 import Card from "../Card"
 import API from "../../utils/API"
 import "./style.css"
 
-class MainComponent extends React.Component {
-    state = {
-        results: [],
-        employeesMatch: [],
-        search: "",
-        category: "name",
-        name: true,
-        email: false
+function MainComponent() {
+    const [results, setResults] = useState([])
+    const [employeesMatch, setEmployeesMatch] = useState([])
+    const [name, setName] = useState(true)
+    const [email, setEmail] = useState(false)
+    const [category, setCategory] = useState("name")
 
-    }
-
-
-
-    componentDidMount = () => {
+    //on load get employees
+    useEffect(() => {
         API.getUsers().then(results => {
-            this.setState({
-                results: results.data.results,
-                employeesMatch: results.data.results
-            })
-        })
+            setResults(results.data.results)
+            setEmployeesMatch(results.data.results)
+        }).catch(err => { console.error(err) })
+    }, [])
 
-    }
+    //when text is entered into input filter through users to find matches
+    function handleInputChange(e) {
 
-    handleInputChange = (e) => {
         let value = e.target.value.toLowerCase()
-        let employees = this.state.results.filter(item => {
-            let category;
+
+        let employees = results.filter(item => {
+            let categoryVar;
             let firstName = item.name.first.toLowerCase()
             let lastName = item.name.last.toLowerCase()
-            if (this.state.category === "name") {
-                category = `${firstName} ${lastName}`
-            } else if (this.state.category === "email") {
 
-                category = item.email
+            if (category === "name") {
+                categoryVar = `${firstName} ${lastName}`
             }
-            if (category.indexOf(value) !== -1) {
+            else if (category === "email") {
+                categoryVar = item.email
+            }
+            if (categoryVar.indexOf(value) !== -1) {
                 return item
             }
         })
-        this.setState({
-            employeesMatch: employees
-        })
+        setEmployeesMatch(employees)
     }
 
-    handleRadio = (e) => {
-        this.setState({
-            category: e.target.value,
-            email: !this.state.email,
-            name: !this.state.name
-        })
+
+    //radio option for search by email or name
+    function handleRadio(e) {
+
+        setCategory(e.target.value)
+        setEmail(!email)
+        setName(!name)
     }
 
-    render() {
-        return (
 
-            <main className="row">
+    return (
 
-                <aside className="col-lg-3 d-lg-block d-none">
-                    <Search handleChange={this.handleInputChange} handleRadio={this.handleRadio} nameCheck={this.state.name} emailCheck={this.state.email} />
-                </aside>
-                <section className="col-lg-8">
-                    <Header />
-                    <div className="row">
-                        <div className="col-12 d-lg-none d-block">
-                            <Search handleChange={this.handleInputChange} handleRadio={this.handleRadio} nameCheck={this.state.name} emailCheck={this.state.email} />
-                        </div>
-                        {this.state.employeesMatch.map(item => {
-                            return <Card name={item} key={item.login.uuid} />
-                        })}
+        <main className="row">
+
+            <aside className="col-lg-3 d-lg-block d-none">
+                <Search handleChange={handleInputChange} handleRadio={handleRadio} nameCheck={name} emailCheck={email} />
+            </aside>
+            <section className="col-lg-8">
+                <Header />
+                <div className="row">
+                    <div className="col-12 d-lg-none d-block">
+                        <Search handleChange={handleInputChange} handleRadio={handleRadio} nameCheck={name} emailCheck={email} />
                     </div>
-                </section>
-            </main>
+                    {employeesMatch.map(item => {
+                        return <Card name={item} key={item.login.uuid} />
+                    })}
+                </div>
+            </section>
+        </main>
 
-        )
-    }
+    )
 }
+
 export default MainComponent;
